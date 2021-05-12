a5d75da33baed054a297bacc146098e3c40eb63b
Ama Nyame-Mensah <ama721@gmail.com>
2021-05-12 07:49:11 -0400

    update to R code and png file

--- Penn SP2 courses word cloud.R
+++ Penn SP2 courses word cloud.R
@@ -16,71 +16,71 @@ sapply(pckgs, FUN = require, character.only = TRUE)
 # function to clean the text up
 clean.text <- function(x)
 {
-  # replace various characters and spaces
-  x = mgsub(x, c("\n","\t", "\r"), c(" ", " ", " "))
-  # remove blank spaces at the beginning
-  x = gsub("^ ", "", x)
-  # remove blank spaces at the end
-  x = gsub(" $", "", x)
+	# replace various characters and spaces
+   x = mgsub(x, c("\n","\t", "\r"), c(" ", " ", " "))
+   # remove blank spaces at the beginning
+   x = gsub("^ ", "", x)
+   # remove blank spaces at the end
+   x = gsub(" $", "", x)
   # double check on the blank spaces
   x = trimws(x, which = c("both", "left", "right"), whitespace = "[ \t\r\n]")
 }
 
 # function to scrape data		
 do_smthn <- function(x){
-  
-  key_omit <- paste(c("Activity:","Course Unit", "Course usually offered", 
-                      "Taught by:", "Prerequisite:", "course offered"), collapse = "|")
-  
-  text_html <- read_html(x)
-  
-  dd <- text_html %>%
-    html_nodes(".courseblockextra") %>%
-    html_text() %>%
-    data.frame() %>%
-    filter(!str_detect(tolower(.), tolower(key_omit))) %>%
-    unlist() %>%
-    clean.text() %>%
-    tolower() %>%
-    paste(., collapse=" ")
+
+key_omit <- paste(c("Activity:","Course Unit", "Course usually offered", 
+									"Taught by:", "Prerequisite:", "course offered"), collapse = "|")
+	
+text_html <- read_html(x)
+
+dd <- text_html %>%
+			html_nodes(".courseblockextra") %>%
+			html_text() %>%
+			data.frame() %>%
+			filter(!str_detect(tolower(.), tolower(key_omit))) %>%
+			unlist() %>%
+			clean.text() %>%
+			tolower() %>%
+			paste(., collapse=" ")
 }
 
 
 #### IMPORT + SCRAPE DATA ####
 # create data frame to store data
 courses <- read_excel("Penn-SP2-courses.xlsx", sheet="courses", col_types = rep("text", times = 3))%>%
-  rowwise() %>%
-  # create data frame to store data
-  mutate(Courses.Text = do_smthn(Courses)) %>%
-  ungroup() %>%
-  data.frame()
+rowwise() %>%
+# create data frame to store data
+mutate(Courses.Text = do_smthn(Courses)) %>%
+ungroup() %>%
+data.frame()
 
 
 #### MANIPULATE CORPUS ####
 # cull corpus of data				
 all <- c(swrk = courses[grep("swrk",tolower(courses$Program)),3], 
-         mssp = courses[grep("mssp",tolower(courses $Program)),3],
-         npl = courses[grep("npl",tolower(courses $Program)),3])
+			mssp = courses[grep("mssp",tolower(courses $Program)),3],
+			npl = courses[grep("npl",tolower(courses $Program)),3])
 # create Corpora
 all_docs <- corpus <- Corpus(VectorSource(all))
 # remove certain strings/numbers
 all_docs <- all_docs  %>%
-  tm_map(., content_transformer(tolower)) %>%
-  tm_map(., removeNumbers) %>%
-  tm_map(., removeWords, c(stopwords("english"), 
-                           "sp2","mssp","swrk","npld", "penn", "university","courses","faculty","will", "advisor",
-                           "requirements", "will", "course", "grade", "students", "student", "week", "activity", "link", 
-                           "links", "readings", "literature","university", "press", "also","assignment", "independent" ,
-                           "assignments", "reading", "class", "absenteeism", "ability", "expectations", "able", 
-                           "using","book", "identify","understand", "understanding", "philadelphia", "expect", 
-                           "expected", "possible", "syllabus", "canvas", "way", "ways", "required", "requires", 
-                           "seminar", "academic", "access", "york", "cambridge", "paper", "project", "seminar", 
-                           "addresses", "address", "journal", "chapter", "nonprofit","pennsylvania","book", "books",
-                           "term","fall", "spring","catalog","unit","offered","lecture", "usually","also", "including",
-                           "include", "taught", "can","units","learn", "either","prerequisite","throughout", "summer",
-                           "well","website", "html",".com", "catalog", "2020-21","2020", "2021")) %>%                        
-  tm_map(., removePunctuation) %>%
-  tm_map(., stripWhitespace)
+tm_map(., content_transformer(tolower)) %>%
+tm_map(., removeNumbers) %>%
+tm_map(., removeWords, c(stopwords("english"), 
+"sp2","mssp","swrk","npld", "penn", "university","courses","faculty","will", "advisor",
+"requirements", "will", "course", "grade", "students", "student", "week", "activity", "link", 
+"links", "readings", "literature","university", "press", "also","assignment", "independent" ,
+"assignments", "reading", "class", "absenteeism", "ability", "expectations", "able", 
+"using","book", "identify","understand", "understanding", "philadelphia", "expect", 
+"expected", "possible", "syllabus", "canvas", "way", "ways", "required", "requires", 
+"seminar", "academic", "access", "york", "cambridge", "paper", "project", "seminar", 
+"addresses", "address", "journal", "chapter", "nonprofit","pennsylvania","book", "books",
+"term","fall", "spring","catalog","unit","offered","lecture", "usually","also", "including",
+"include", "taught", "can","units","learn", "either","prerequisite","throughout", "summer",
+"well","website", "html",".com", "catalog", "2020-21","2020", "2021")) %>%
+tm_map(., removePunctuation) %>%
+tm_map(., stripWhitespace)
 
 # create term-document matrix
 tdm <- TermDocumentMatrix(all_docs)
@@ -91,7 +91,7 @@ tdm <- as.matrix(tdm) %>% data.frame()
 #### VISUALIZE DATA ####
 #  separate clouds same plot
 dev.new(width=10, height=6, unit="in")
-png(file="SP2 Course Descriptions2.png", width = 12, height = 6, units = 'in', res = 300, bg = "white")
+png(file="SP2 Course Descriptions2.png", width = 12, height = 6, units = 'in', res = 300, bg = "transparent")
 par(mfrow=c(1,3))
 #Create word cloud of mssp courses
 set.seed(0721)
