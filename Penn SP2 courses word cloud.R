be07522deee5439a89404739dc3e0fe700b305a5
Ama Nyame-Mensah <ama721@gmail.com>
2021-05-12 08:02:21 -0400

    update r code

--- Penn SP2 courses word cloud.R
+++ Penn SP2 courses word cloud.R
@@ -6,14 +6,14 @@
 pckgs <- c("tidyverse","readxl","rvest", "tm", "wordcloud","stringr","mgsub")
 
 if (any(pckgs %notin% rownames(installed.packages())==TRUE)){
-  install.packages(pckgs, repos = c(CRAN = "http://cloud.r-project.org"))}
+install.packages(pckgs, repos = c(CRAN = "http://cloud.r-project.org"))}
 
 # Load packages
 sapply(pckgs, FUN = require, character.only = TRUE)
 
 
 ##### FUNCTIONS ####
-# function to clean the text up
+## function to clean the text up
 clean.text <- function(x)
 {# replace various characters and spaces
 x = mgsub(x, c("\n","\t", "\r"), c(" ", " ", " "))
@@ -25,7 +25,7 @@ x = gsub(" $", "", x)
 x = trimws(x, which = c("both", "left", "right"), whitespace = "[ \t\r\n]")
 }
 
-# function to scrape data		
+## function to scrape data		
 do_smthn <- function(x){
 
 key_omit <- paste(c("Activity:","Course Unit", "Course usually offered", 
@@ -50,18 +50,18 @@ dd <-
 # create data frame to store data
 courses <- read_excel("Penn-SP2-courses.xlsx", sheet="courses", col_types = rep("text", times = 3))%>%
 rowwise() %>%
-# create data frame to store data
+# scrape data and place in the appropriate column
 mutate(Courses.Text = do_smthn(Courses)) %>%
 ungroup() %>%
 data.frame()
 
 
 #### MANIPULATE CORPUS ####
-# cull corpus of data				
+# place data into a named character vector with three elements		
 all <- 
-  c(swrk = courses[grep("swrk",tolower(courses$Program)),3], 
-	mssp = courses[grep("mssp",tolower(courses $Program)),3],
-	npl = courses[grep("npl",tolower(courses $Program)),3])
+c(swrk = courses[grep("swrk",tolower(courses$Program)),3], 
+mssp = courses[grep("mssp",tolower(courses $Program)),3],
+npl = courses[grep("npl",tolower(courses $Program)),3])
 
 # create corpus
 all_docs <- Corpus(VectorSource(all))
@@ -95,18 +95,18 @@ tdm <- as.matrix(tdm) %>% data.frame()
 
 
 #### VISUALIZE DATA ####
-#  three separate clouds on the same page
+# three separate clouds on the same page
 dev.new(width=10, height=6, unit="in")
 png(file="SP2 Course Descriptions2.png", width = 12, height = 6, units = 'in', res = 300, bg = "transparent")
 par(mfrow=c(1,3))
-#Create word cloud of mssp courses
+# Create word cloud of mssp courses
 set.seed(0721)
 wordcloud(rownames(tdm), tdm$mssp, min.freq = 3, scale=c(6, .08), random.order = FALSE, 
 rot.per = 0, max.words = 50,  random.color = FALSE, colors= c("indianred"))
-#Create word cloud of social work courses
+# Create word cloud of social work courses
 wordcloud(rownames(tdm), tdm$swrk, min.freq = 3, scale=c(6, .08), random.order = FALSE, 
 rot.per = 0, max.words = 50, random.color = FALSE, colors= c("#405AAF"))
-#Create word cloud of nonprofit leadership courses
+# Create word cloud of nonprofit leadership courses
 wordcloud(rownames(tdm), tdm$npl, min.freq = 3, scale=c(6, .08), random.order = FALSE, 
 rot.per = 0, max.words = 50, random.color = FALSE, colors= c("#595959"))
 dev.off(); dev.off()
